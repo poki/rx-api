1441965c652ccb45302c3dbf1d46dc9cbb261f7c
Prokos <awsumthx@gmail.com>
2019-11-29 11:04:27 +0100

    Options need to come from the fetchAction

--- src/createApiEpic.js
+++ src/createApiEpic.js
@@ -78,8 +78,8 @@ export default function createApiEpic(id, handler, getCBStream) {
 							crossDomain: true,
 							progressSubscriber: progressSubscriber$,
 						}).pipe(
-							switchMap(result => of(success({ options: action.payload.options, result }))),
-							catchError(result => of(error({ options: action.payload.options, result }))),
+							switchMap(result => of(success({ options: fetchAction.payload.options, result }))),
+							catchError(result => of(error({ options: fetchAction.payload.options, result }))),
 						),
 					).pipe(
 						takeUntil(cancel$),
